ea89c25ffbf99f80d7fe1a0aa9eacbd54c2e8598
vicpada <vicpada@gmail.com>
2015-07-14 18:48:15 +0200

    Initial structure for the test added

---
+++ src/main/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 14/07/2015
+ * Time: 18:16
+ */
---
+++ src/test/php/com.realexpayments.remote.sdk/domain.payment/CardTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 14/07/2015
+ * Time: 18:47
+ */
+
+namespace php\com\realexpayments\remote\sdk\domain\payment;
+
+
+class CardTest extends \PHPUnit_Framework_TestCase {
+
+}
