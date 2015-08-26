3020322c4569012bbbfa2a6aa29641acf17c1409
Graham Whelan <graham.whelan@softwaredesign.ie>
2015-08-26 19:06:46 +0100

    Require country code when setting AVS details
    When a developer is setting AVS details through the
    addAddressVerificationServiceDetails method, they are now
    required to specify a country value that is used to populate the
    TSS info.

--- test/main/php/com-realexpayments-remote-sdk/domain-payment/PaymentRequestTest.php
+++ test/main/php/com-realexpayments-remote-sdk/domain-payment/PaymentRequestTest.php
@@ -19,14 +19,16 @@ class PaymentRequestTest extends \PHPUnit_Framework_TestCase {
 		//test variations of address and postcode with TSS Info field null
 		$addressLine         = "123 Fake St";
 		$postcode            = "WB1 A42";
+		$country             = "GB";
 		$expectedBillingCode = "142|123";
 		$request             = new PaymentRequest();
-		$request->addAddressVerificationServiceDetails( $addressLine, $postcode );
+		$request->addAddressVerificationServiceDetails( $addressLine, $postcode, $country );
 
 		$addresses = $request->getTssInfo()->getAddresses();
 
 		$this->assertEquals( $expectedBillingCode, $addresses[0]->getCode() );
 		$this->assertEquals( AddressType::BILLING, $addresses[0]->getType() );
+		$this->assertEquals( $country, $addresses[0]->getCountry() );
 	}
 
 	/**
@@ -35,13 +37,15 @@ class PaymentRequestTest extends \PHPUnit_Framework_TestCase {
 	public function  testAddAddressVerificationServiceDetails2() {
 		$addressLine         = "123 5 Fake St";
 		$postcode            = "1WB 5A2";
+		$country             = "GB";
 		$expectedBillingCode = "152|1235";
 		$request             = new PaymentRequest();
-		$request->addAddressVerificationServiceDetails( $addressLine, $postcode );
+		$request->addAddressVerificationServiceDetails( $addressLine, $postcode, $country );
 
 		$addresses = $request->getTssInfo()->getAddresses();
 		$this->assertEquals( $expectedBillingCode, $addresses[0]->getCode() );
 		$this->assertEquals( AddressType::BILLING, $addresses[0]->getType() );
+		$this->assertEquals( $country, $addresses[0]->getCountry() );
 	}
 
 	/**
@@ -50,13 +54,15 @@ class PaymentRequestTest extends \PHPUnit_Framework_TestCase {
 	public function  testAddAddressVerificationServiceDetails3() {
 		$addressLine         = "Apt 15, 123 Fake St";
 		$postcode            = "ABC 5A2";
+		$country             = "GB";
 		$expectedBillingCode = "52|15123";
 		$request             = new PaymentRequest();
-		$request->addAddressVerificationServiceDetails( $addressLine, $postcode );
+		$request->addAddressVerificationServiceDetails( $addressLine, $postcode, $country );
 
 		$addresses = $request->getTssInfo()->getAddresses();
 		$this->assertEquals( $expectedBillingCode, $addresses[0]->getCode() );
 		$this->assertEquals( AddressType::BILLING, $addresses[0]->getType() );
+		$this->assertEquals( $country, $addresses[0]->getCountry() );
 	}
 
 	/**
@@ -65,13 +71,15 @@ class PaymentRequestTest extends \PHPUnit_Framework_TestCase {
 	public function  testAddAddressVerificationServiceDetails4() {
 		$addressLine         = "Fake St";
 		$postcode            = "AI10 9AB";
+		$country             = "GB";
 		$expectedBillingCode = "109|";
 		$request             = new PaymentRequest();
-		$request->addAddressVerificationServiceDetails( $addressLine, $postcode );
+		$request->addAddressVerificationServiceDetails( $addressLine, $postcode, $country );
 
 		$addresses = $request->getTssInfo()->getAddresses();
 		$this->assertEquals( $expectedBillingCode, $addresses[0]->getCode() );
 		$this->assertEquals( AddressType::BILLING, $addresses[0]->getType() );
+		$this->assertEquals( $country, $addresses[0]->getCountry() );
 	}
 
 
@@ -81,13 +89,15 @@ class PaymentRequestTest extends \PHPUnit_Framework_TestCase {
 	public function  testAddAddressVerificationServiceDetails5() {
 		$addressLine         = "30 Fake St";
 		$postcode            = "";
+		$country             = "GB";
 		$expectedBillingCode = "|30";
 		$request             = new PaymentRequest();
-		$request->addAddressVerificationServiceDetails( $addressLine, $postcode );
+		$request->addAddressVerificationServiceDetails( $addressLine, $postcode, $country );
 
 		$addresses = $request->getTssInfo()->getAddresses();
 		$this->assertEquals( $expectedBillingCode, $addresses[0]->getCode() );
 		$this->assertEquals( AddressType::BILLING, $addresses[0]->getType() );
+		$this->assertEquals( $country, $addresses[0]->getCountry() );
 	}
 
 
@@ -97,14 +107,16 @@ class PaymentRequestTest extends \PHPUnit_Framework_TestCase {
 	public function  testAddAddressVerificationServiceDetailsWithTssInfo() {
 		$addressLine         = "123 Fake St";
 		$postcode            = "WB1 A42";
+		$country             = "";
 		$expectedBillingCode = "142|123";
 		$request             = new PaymentRequest();
 		$request->addTssInfo( new TssInfo() )
-		        ->addAddressVerificationServiceDetails( $addressLine, $postcode );
+		        ->addAddressVerificationServiceDetails( $addressLine, $postcode, $country );
 
 		$addresses = $request->getTssInfo()->getAddresses();
 		$this->assertEquals( $expectedBillingCode, $addresses[0]->getCode() );
 		$this->assertEquals( AddressType::BILLING, $addresses[0]->getType() );
+		$this->assertEquals( $country, $addresses[0]->getCountry() );
 	}
 
 
